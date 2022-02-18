e7d344e5c8397f039809c25606fca9de0cc0b55f
Ylerson Almonte <yacwarefree1@gmail.com>
2022-02-17 22:18:36 -0400

    Add Loading Spinner

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CSpinner } from '@coreui/react'
+
+
+export const Loading = () => {
+    return (
+        <div className='main_login'>
+
+            <CSpinner variant="grow"/>
+
+        </div>
+    );
+};
